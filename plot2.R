56693edd70a64aa9f25d7b19acb0b68f821a661b
Andreas Stenmark <andreas.stenmark@gmail.com>
2014-06-06 13:26:52 +0200

    Scripts and plot files

---
+++ plot2.R
@@ -0,0 +1,45 @@
+# plot2.R
+
+dataarch <- "household_power_consumption.zip"
+datafile <- "household_power_consumption.txt"
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+colclasses <- c("character",
+                "character",
+                "numeric",
+                "numeric",
+                "numeric",
+                "numeric",
+                "numeric",
+                "numeric",
+                "numeric")
+
+# Download the file if it doesn't exist
+if (!file.exists(dataarch))
+    download.file(url, dataarch, method = "curl")
+
+# Read the data file
+data <- read.csv(unz(dataarch, datafile),
+                 nrows = 2075260,
+                 comment.char = "",
+                 colClasses = colclasses,
+                 stringsAsFactors = FALSE,
+                 sep = ";", na.strings = "?")
+
+# Convert Date and Time columns to something useful
+data$TimeStamp <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S", tz = "US/Pacific")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Get data from 2007-02-01 and 2007-02-02
+subdata <- data[data$Date == as.Date("2007-02-01") | data$Date == as.Date("2007-02-02"),]
+
+# Plot the data
+lct <- Sys.getlocale("LC_TIME")
+Sys.setlocale("LC_TIME", "C")
+png("plot2.png")
+plot(subdata$TimeStamp, subdata$Global_active_power,
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)",
+     type = "l")
+dev.off()
+Sys.setlocale("LC_TIME", lct)
+
---
+++ plot4.R
@@ -0,0 +1,62 @@
+# plot4.R
+
+dataarch <- "household_power_consumption.zip"
+datafile <- "household_power_consumption.txt"
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+colclasses <- c("character",
+                "character",
+                "numeric",
+                "numeric",
+                "numeric",
+                "numeric",
+                "numeric",
+                "numeric",
+                "numeric")
+
+# Download the file if it doesn't exist
+if (!file.exists(dataarch))
+    download.file(url, dataarch, method = "curl")
+
+# Read the data file
+data <- read.csv(unz(dataarch, datafile),
+                 nrows = 2075260,
+                 comment.char = "",
+                 colClasses = colclasses,
+                 stringsAsFactors = FALSE,
+                 sep = ";", na.strings = "?")
+
+# Convert Date and Time columns to something useful
+data$TimeStamp <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S", tz = "US/Pacific")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Get data from 2007-02-01 and 2007-02-02
+subdata <- data[data$Date == as.Date("2007-02-01") | data$Date == as.Date("2007-02-02"),]
+
+# Plot the data
+lct <- Sys.getlocale("LC_TIME")
+Sys.setlocale("LC_TIME", "C")
+png("plot4.png")
+par(mfrow = c(2, 2))
+
+# Top left
+plot(subdata$TimeStamp, subdata$Global_active_power,
+     xlab = "", ylab = "Global Active Power", type = "l")
+
+# Top right
+plot(subdata$TimeStamp, subdata$Voltage,
+     xlab = "datetime", ylab = "Voltage", type = "l")
+
+# Bottom left
+plot(subdata$TimeStamp, subdata$Sub_metering_1,
+     xlab = "", ylab = "Energy sub metering", type = "l")
+lines(subdata$TimeStamp, subdata$Sub_metering_2, col = "red")
+lines(subdata$TimeStamp, subdata$Sub_metering_3, col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"), bty = "n")
+
+# Bottom right
+plot(subdata$TimeStamp, subdata$Global_reactive_power,
+     xlab = "datetime", ylab = "Global_reactive_power", type = "l")
+
+dev.off()
+Sys.setlocale("LC_TIME", lct)
+
